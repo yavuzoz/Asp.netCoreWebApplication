262c16fcfd7dd740949c0b5aa15e555d40a98d3c
Yavuz Özbay <ozbayavz@gmail.com>
2023-07-19 22:03:48 +0200

    Add project files.

---
+++ Asp.netCoreWebApplication/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
+
+app.Run();
